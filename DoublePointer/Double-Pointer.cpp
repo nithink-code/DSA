22fe2267e8b7c74c3ae6eadb97fc9ffc71e8fcc4
nithin <nithinkokkapuni@gmail.com>
2024-09-17 22:10:01 +0530

    DoublePointer

---
+++ DoublePointer/Double-Pointer.cpp
@@ -0,0 +1,22 @@
+// Double Pointer usage
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=5;
+  int *p=&a;
+  int **ptr=&p; // Double Pointer
+  // To print value of a using 3 methods
+  cout<<"Value is ->"<<*p<<endl;
+  cout<<"Value is->"<<**ptr<<endl;
+  cout<<"Value is->"<<a<<endl; 
+
+  // To print the address of a in 3 methods
+
+  cout<<"Value is->"<<&a<<endl;
+  cout<<"Value is->"<<p<<endl;
+  cout<<"Value is ->"<<*ptr<<endl;  
+  return 0;
+}
+*/
